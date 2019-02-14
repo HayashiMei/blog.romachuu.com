ee854672e36d54d48e7819f75f95f87be6baafd4
HayashiMei <hayashi@kato-megumi.com>
2019-02-14 18:04:44 +0800

    Site updated: 2019-02-14 18:04:42

--- sw.js
+++ sw.js
@@ -1,97 +1,97 @@
-"use strict";
-(function () {
-  var cacheVersion = "20180613";
-  var staticImageCacheName = "image" + cacheVersion;
-  var staticAssetsCacheName = "assets" + cacheVersion;
-  var contentCacheName = "content" + cacheVersion;
-  var vendorCacheName = "vendor" + cacheVersion;
+'use strict';
+(function() {
+  var cacheVersion = '20180613';
+  var staticImageCacheName = 'image' + cacheVersion;
+  var staticAssetsCacheName = 'assets' + cacheVersion;
+  var contentCacheName = 'content' + cacheVersion;
+  var vendorCacheName = 'vendor' + cacheVersion;
   var maxEntries = 100;
 
-  self.importScripts("lib/sw-toolbox/sw-toolbox.js");
+  self.importScripts('lib/sw-toolbox/sw-toolbox.js');
   self.toolbox.options.debug = false;
   self.toolbox.options.networkTimeoutSeconds = 3;
 
-  self.toolbox.router.get("/images/(.*)", self.toolbox.cacheFirst, {
+  self.toolbox.router.get('/images/(.*)', self.toolbox.cacheFirst, {
     cache: {
       name: staticImageCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
   self.toolbox.router.get('/js/(.*)', self.toolbox.cacheFirst, {
     cache: {
       name: staticAssetsCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
   self.toolbox.router.get('/css/(.*)', self.toolbox.cacheFirst, {
     cache: {
       name: staticAssetsCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
-  self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+  self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
     origin: /cdn\.jsdelivr\.net/,
     cache: {
       name: staticAssetsCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
-  self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+  self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
     origin: /cdnjs\.loli\.net/,
     cache: {
       name: staticAssetsCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
-  self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+  self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
     origin: /fonts\.loli\.net/,
     cache: {
       name: staticAssetsCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
-  self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+  self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
     origin: /gstatic\.loli\.net/,
     cache: {
       name: staticAssetsCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
-  self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
+  self.toolbox.router.get('/(.*)', self.toolbox.cacheFirst, {
     origin: /cdn\.bootcss\.com/,
     cache: {
       name: staticAssetsCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
-  self.toolbox.router.get("/(.*)", self.toolbox.networkOnly, {
+  self.toolbox.router.get('/(.*)', self.toolbox.networkOnly, {
     origin: /(www\.google-analytics\.com|ssl\.google-analytics\.com)/,
     cache: {
       name: vendorCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
   self.toolbox.router.get('/*', self.toolbox.networkFirst, {
     cache: {
       name: contentCacheName,
-      maxEntries: maxEntries
-    }
+      maxEntries: maxEntries,
+    },
   });
 
-  self.addEventListener("install", function (event) {
-    return event.waitUntil(self.skipWaiting())
+  self.addEventListener('install', function(event) {
+    return event.waitUntil(self.skipWaiting());
   });
 
-  self.addEventListener("activate", function (event) {
-    return event.waitUntil(self.clients.claim())
-  })
+  self.addEventListener('activate', function(event) {
+    return event.waitUntil(self.clients.claim());
+  });
 })();
