62d19cea95859c5dd14b45228831a24dab23b91a
HayashiMei <hayashi@kato-megumi.com>
2019-09-04 11:17:00 +0800

    Site updated: 2019-09-04 11:17:00

--- sw.js
+++ sw.js
@@ -2,56 +2,53 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+  const cacheVersion = 'v20190904';
+
+  workbox.setConfig({ debug: false });
+  workbox.core.setCacheNameDetails({
+    prefix: 'blog.romachuu.com',
+    suffix: 'v20190904',
+  });
+
+  workbox.routing.registerRoute(
+    /\/css\/(.*)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: `css-${cacheVersion}`,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+        }),
+      ],
+    })
+  );
+
+  workbox.routing.registerRoute(
+    /\/js\/(.*)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: `js-${cacheVersion}`,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+        }),
+      ],
+    })
+  );
+
+  workbox.routing.registerRoute(
+    /\/images\/(.*)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: `images-${cacheVersion}`,
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+        }),
+      ],
+    })
+  );
+
+  workbox.routing.registerRoute(/(cdn\.jsdelivr\.net|.*\.loli\.net|cdn\.bootcss\.com|(www|ssl)\.google-analytics\.com)\/(.*)$/, new workbox.strategies.StaleWhileRevalidate());
+
+  workbox.routing.registerRoute(/\/*$/, new workbox.strategies.NetworkFirst());
 } else {
-  return console.log(`Boo! Workbox didn't load 😬`);
+  console.log(`Boo! Workbox didn't load 😬`);
 }
-
-workbox.precaching.precacheAndRoute([]);
-
-const cacheVersion = 'v20190904';
-
-workbox.setConfig({ debug: false });
-workbox.core.setCacheNameDetails({
-  prefix: 'blog.romachuu.com',
-  suffix: 'v20190904',
-});
-
-workbox.routing.registerRoute(
-  /\/css\/(.*)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: `css-${cacheVersion}`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-      }),
-    ],
-  })
-);
-
-workbox.routing.registerRoute(
-  /\/js\/(.*)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: `js-${cacheVersion}`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-      }),
-    ],
-  })
-);
-
-workbox.routing.registerRoute(
-  /\/images\/(.*)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: `images-${cacheVersion}`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-      }),
-    ],
-  })
-);
-
-workbox.routing.registerRoute(/(cdn\.jsdelivr\.net|.*\.loli\.net|cdn\.bootcss\.com|(www|ssl)\.google-analytics\.com)\/(.*)$/, new workbox.strategies.StaleWhileRevalidate());
-
-workbox.routing.registerRoute(/\/*$/, new workbox.strategies.NetworkFirst());
